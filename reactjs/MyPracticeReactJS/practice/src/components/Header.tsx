d040dfdb036e330bbf7868bfaf32bd5950b3d296
Huynh Minh Tam <nu330006@gmai.com>
2024-02-22 18:20:56 +0700

    cccchange

---
+++ reactjs/MyPracticeReactJS/practice/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <>
+            <section>
+                <a>Minh Tam</a>
+                <ul>
+                    <li>Residences</li>
+                    <li>Our Value</li>
+                    <li>Contact us</li>
+                    <li>Get Started</li>
+                    <li>Contact</li>
+                </ul>
+            </section>
+        </>
+    )
+}
